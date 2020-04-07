84abf4c9bd188eeaf0f1a99bb40c7be9bd1c36eb
Renato Avelino <43862847+RenatoAvelino@users.noreply.github.com>
2020-04-07 14:32:24 -0300

    Prefabs and first tests
    Board Prefab, Ball Prefab, Baton Prefab, GameController Script, BoardTests Script.

---
+++ Pong/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject batonPrefab;
+    void Start()
+    {
+        batonPrefab = Resources.Load<GameObject>("Prefabs/Baton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject[] CreateBatons()
+    {
+        GameObject[] batons = new GameObject[2]; //Create the Array
+        for(int i = 0; i < 2; i++){
+            GameObject temp = GameObject.Instantiate(batonPrefab);
+            temp.transform.position = new Vector3((7 - (14 * i)), 0.5f, 0);//Set the initial position of the batons
+            batons[i] = temp;
+        }
+        return batons; //Return the Array
+    }
+}
---
+++ Pong/Assets/Tests/TestsPlay/BoardTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BoardTests
+    {
+        // A Test behaves as an ordinary method
+        GameController script;
+        GameObject board;
+        [SetUp]
+        public void Setup()
+        {
+            GameObject tmp = (GameObject)Resources.Load("Prefabs/Board");
+            board = GameObject.Instantiate(tmp);
+            script = board.GetComponent<GameController>();
+        }
+
+        [Test]
+        public void BoardTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void One_Baton_Is_Succesfully_Created() //Test if the Array has been created
+        {
+            GameObject[] batons = script.CreateBatons();
+
+            Assert.IsNotNull(batons); //Test if the Array exists
+        }
+
+        [Test]
+        public void Both_Batons_Are_Succesfully_Created() //Test the creation of both batons 
+        {
+            GameObject[] batons = script.CreateBatons();
+
+            Assert.AreEqual(2, batons.Length); //Test if the Array has 2 batons 
+        }
+
+        [Test]
+        public void Ball_Is_Succesfully_Created() //Test the creation of the ball
+        {
+            GameObject ball = script.CreateBall();
+
+            Assert.IsNotNull(ball); //Test if the ball has been created
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator BoardTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
